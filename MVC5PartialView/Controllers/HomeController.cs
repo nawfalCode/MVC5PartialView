58a333a3c43a3eddf9b2730735d2df2f92ebf676
Nawfal Mahdi <nawfal.mahdi@hotmail.com>
2014-07-22 06:25:19 +0400

    MVC5PartialView
    This project retrieves data from database and render it using Partial
    View....

---
+++ MVC5PartialView/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5PartialView.Controllers
+{
+
+   
+
+
+    public class HomeController : Controller
+    {
+        public ActionResult ViewwithPartialData()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        /// 
+
+        public ActionResult PartialView()
+        {
+           using(UsersEntities u=new UsersEntities())
+           {
+               var v = u.cars.OrderBy(a => a.Id).ToList();
+               return PartialView("_partialView", v);
+           }
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MVC5PartialView/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC5PartialView.Startup))]
+namespace MVC5PartialView
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
